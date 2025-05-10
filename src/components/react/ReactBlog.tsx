2239c3402197be4973bfb51692c659050f6fb88d
Keyru Nasir <keyrsyntax@gmail.com>
2025-05-10 18:52:27 +0300

    React component added

--- src/components/react/ReactBlog.tsx
+++ src/components/react/ReactBlog.tsx
@@ -4,7 +4,10 @@ import React from 'react'
 
 const ReactBlog = () => {
   return (
-    <div>ReactBlogComponent</div>
+   <div className='flex flex-row w- mx-auto mt-10 justify-between items-center gap-10 '> 
+    <div className='border mx-auto w-[40%] px-4 py-10'>ReactBlogComponent</div>
+    <div className='border mx-auto w-[40%] px-4 py-10'>ReactBlogComponent</div>
+   </div>
   )
 }
 
